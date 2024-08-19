bc008ed7493465dbbdd20b93e3475e0424e6a4cd
Mateus <mateusb.a27@gmail.com>
2024-08-19 20:15:30 -0300

    Luminaria

--- Luminaria.java
+++ Luminaria.java
@@ -13,4 +13,12 @@ public class Luminaria {
     public void ajustarIntensidade(int intensidade) {
         this.intensidade = intensidade;
     }
+
+    public boolean getEstado() {
+        return this.estado;
+    }
+
+    public int getIntensidade() {
+        return this.intensidade;
+    }
 }
